861d949b3d138e02b809d87a6cd7a51c2930d300
Erick <erick-patrickfreitas@outlook.com>
2026-04-12 12:26:23 -0300

    Html base + telas de usuários

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,10 @@
 class DashboardController < ApplicationController
-    
   def index
     @total_tickets = Ticket.count
     @open_tickets = Ticket.joins(:ticket_status).where(ticket_statuses: { is_final: false }).count
     @closed_tickets = Ticket.joins(:ticket_status).where(ticket_statuses: { is_final: true }).count
     @total_blocks = Block.count
     @total_units = Unit.count
+    @recent_tickets = policy_scope(Ticket).order(created_at: :desc).limit(5)
   end
 end
